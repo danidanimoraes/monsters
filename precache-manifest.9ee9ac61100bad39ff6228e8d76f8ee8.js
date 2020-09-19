a7b201c3255086e0b3bdaffe6c21722436a93082
Daniele de Moraes <danimoraes.barbosa@gmail.com>
2020-09-19 16:15:16 -0300

    Updates

---
+++ precache-manifest.9ee9ac61100bad39ff6228e8d76f8ee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2abf9fe378be8d5e07d422f5927f0f3c",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c3be2985c8a36d6c320e",
+    "url": "/monsters/static/css/main.e7b1ff9f.chunk.css"
+  },
+  {
+    "revision": "e485dc02b6edcc1aee03",
+    "url": "/monsters/static/js/2.1ca8b328.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/monsters/static/js/2.1ca8b328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3be2985c8a36d6c320e",
+    "url": "/monsters/static/js/main.157e50de.chunk.js"
+  },
+  {
+    "revision": "0b71497f43c75273b692",
+    "url": "/monsters/static/js/runtime-main.da15c421.js"
+  }
+]);
